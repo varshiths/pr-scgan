c132269c13061e9ef49239a117ac60c1f56ee15e
Varshith S <vsreeramdass@jp.honda-ri.com>
2018-05-29 17:04:27 +0900

    slerp interpolation

--- utils/tests.py
+++ utils/tests.py
@@ -64,4 +64,14 @@ def interpolate(point1, point2, n):
 	return np.stack(points, axis=0)
 
 def spher_interpolate(point1, point2, n):
-	return interpolate(point1, point2, n)
+	val = np.linspace(0, 1, n+1)
+	points = []
+	for i in range(n+1):
+		points.append(slerp(val[i], point1, point2))
+
+	return np.stack(points, axis=0)
+
+def slerp(val, low, high):
+    omega = np.arccos(np.dot(low/np.linalg.norm(low), high/np.linalg.norm(high)))
+    so = np.sin(omega)
+    return np.sin((1.0-val)*omega) / so * low + np.sin(val*omega)/so * high
