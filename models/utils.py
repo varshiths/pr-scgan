86b4c881b2640bafbaa9cff44503bff4525704cc
Varshith S <vsreeramdass@jp.honda-ri.com>
2018-06-22 11:41:22 +0900

    soft dtw added

--- models/utils.py
+++ models/utils.py
@@ -1,5 +1,36 @@
 import tensorflow as tf
+import numpy as np
 import math
+
+def np_min(arr, gamma=1.0):
+    '''
+    Implements soft min among a list of tensors x = [x1, x2, ...]
+    of dimensions [b] each returns a [b] sized tensor
+    '''
+    arr = np.stack(arr)
+    return -gamma * np.log( np.sum( np.exp(-arr/gamma), axis=0) )
+
+def np_dtw(x1, x2):
+    b , t1, s  = x1.shape
+    b_, t2, s_ = x2.shape
+    assert b == b_, "Batch sizes are not the same"
+
+    r = np.zeros((b, t1+1, t2+1))
+    r[:, 1:, :] = math.inf
+    r[:, :, 1:] = math.inf
+    r[:, 0, 0] = 0
+
+    for i in range(1, t1+1):
+        for j in range(1, t2+1):
+            cost = np.linalg.norm(x1[:, i-1]-x2[:, j-1], axis=1)
+            val = cost + hard_min([
+                    r[:, i-1, j  ],
+                    r[:, i  , j-1],
+                    r[:, i-1, j-1]
+                ], gamma=1.0)
+            r[:, i, j] = val
+
+    return r[:, t1, t2]
 
 def soft_min(arr, gamma=1.0):
     '''
@@ -18,20 +49,37 @@ def soft_dtw(x1, x2):
     b_, t2, s_ = x2.shape.as_list()
     assert b == b_, "Batch sizes are not the same"
 
-    r = tf.zeros((b, t1+1, t2+1))
+    r = np.zeros((b, t1+1, t2+1))
     r[:, 1:, :] = math.inf
     r[:, :, 1:] = math.inf
+    r[:, 0, 0] = 0.0
+    r = tf.Variable(r, dtype=tf.float32)
 
     for i in range(1, t1+1):
         for j in range(1, t2+1):
-            cost = tf.norm(x1[:, i]-x2[:, j], axis=1)
-            r[:, i, j] = cost + soft_min([
+            cost = tf.norm(x1[:, i-1]-x2[:, j-1], axis=1)
+            val = cost + soft_min([
                     r[:, i-1, j  ],
                     r[:, i  , j-1],
                     r[:, i-1, j-1]
                 ], gamma=1.0)
+            indices = np.zeros((b, 3), dtype=np.int32)
+            indices[:, 0] = np.arange(b)
+            indices[:, 1] = i
+            indices[:, 2] = j
+            r = tf.scatter_nd_update(r, indices, val)
 
     return r[:, t1, t2]
 
 if __name__ == '__main__':
-    import pdb; pdb.set_trace()
+
+    x1 = tf.random_normal((10,3,32))
+    x2 = tf.random_normal((10,4,32))
+
+    temp = soft_dtw(x1, x2)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        val = temp.eval(session=sess)
+        import pdb; pdb.set_trace()
+        print()
