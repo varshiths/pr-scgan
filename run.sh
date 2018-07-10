21da6cf8a34c58c94a91734940db90f8f74b1f9d
Varshith S <vsreeramdass@jp.honda-ri.com>
2018-07-10 13:50:42 +0900

    fix smoothness weight

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ time python3 main.py \
 	--architecture cseqgan \
 	--config configs/csg_dz_s.json \
 	--dataset jslads \
-	--mode train \
-	# --test_index 5 \
+	--mode test \
+	--test_index 5 \
 	# --model dvyi0 \
 	# --save dvyi0 \
 	# --log \
