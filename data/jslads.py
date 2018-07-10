a114f3da2746a62ff649428502cc312df2b022c1
Varshith S <vsreeramdass@jp.honda-ri.com>
2018-07-10 13:47:50 +0900

    fix mean position of nslct joints

--- data/jslads.py
+++ data/jslads.py
@@ -36,7 +36,7 @@ class JSLADS(JSLAD):
 		jp = self.get_joint_indices_partition(self.config.sections)
 		gestures_slct = gestures[:, :, jp[0], :]
 		# gestures_nslct = gestures[0, 0, jp[1], :]
-		gestures_nslct = np.mean(gestures[:, :, jp[1], :], axis=0,1)
+		gestures_nslct = np.mean(gestures[:, :, jp[1], :], axis=(0,1))
 
 		# encode words into one hot encodings
 		annotations, ann_lengths = self.process_input(sentences, indices_of_words)
